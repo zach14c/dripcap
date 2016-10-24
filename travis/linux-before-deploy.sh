f14f77facbacd164768d2cbdb04ea6b41855b44b
Ron Hashimoto <mail@h2so5.net>
2016-10-24 21:19:19 +0900

    rewrite electron's rpath before dpkg-deb

--- travis/linux-before-deploy.sh
+++ travis/linux-before-deploy.sh
@@ -1,3 +1,8 @@
-npm run dpkg
+chmod -R g-w debian
+gulp debian
+cd .debian
+chrpath -r /usr/share/dripcap ./usr/share/dripcap/dripcap
+fakeroot dpkg-deb --build . ../dripcap-linux-amd64.deb
+cd ..
 fakeroot alien --to-rpm -k --scripts dripcap-linux-amd64.deb
 mv *.rpm dripcap-linux-amd64.rpm
