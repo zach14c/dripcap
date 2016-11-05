65541edd900dc947c84cb9252a5fa735a99e8965
Ron Hashimoto <mail@h2so5.net>
2016-11-05 13:00:25 +0900

    install node 7.x via nvm

--- travis/linux-before-install.sh
+++ travis/linux-before-install.sh
@@ -1,9 +1,10 @@
-rm -rf ~/.nvm
+rm -rf ~/.nvm && git clone https://github.com/creationix/nvm.git ~/.nvm
+(cd ~/.nvm && git checkout `git describe --abbrev=0 --tags`)
+source ~/.nvm/nvm.sh
+nvm install 7
+
 sudo apt-key adv --keyserver pgp.mit.edu --recv D101F7899D41F3C3
 echo "deb http://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-
-curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
-sudo apt-get install -y nodejs yarn
 
 export CC="gcc-5"
 export CXX="g++-5"
--- travis/osx-before-install.sh
+++ travis/osx-before-install.sh
@@ -1,5 +1,7 @@
 brew update
-brew install nvm gpg jq node yarn
+brew install gpg jq
+nvm install 7
+
 export PATH=/usr/local/opt/gnupg/libexec/gpgbin:$PATH
 export CC=clang
 export CXX=clang++
